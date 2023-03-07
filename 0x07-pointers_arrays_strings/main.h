e7ccfdb71fe90af6f945c9ef13882ca0c09009ae
Phakoth <phakoth@gmail.com>
2023-03-07 14:37:13 -0800

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,6 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
+
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -10,3 +12,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 #endif
+
